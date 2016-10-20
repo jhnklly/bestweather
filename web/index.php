929f0e692c344a7836a7bff936dab033fb5630d1
john-m-kelly <john_m_kelly@apple.com>
2016-10-20 11:16:39 -0700

    add encodeuri function

--- web/index.php
+++ web/index.php
@@ -117,3 +117,10 @@ $app->get('/dbupdate/', function() use($app) {
 });
 
 $app->run();
+
+
+function encodeURIComponent($str) {
+    $revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
+    return strtr(rawurlencode($str), $revert);
+}
+
